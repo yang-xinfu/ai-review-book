400b66d56418c432186bad9e695c9ce2d9ccbced
yangxinfu <yangxinfu@53zaixian.com>
2025-04-21 10:19:07 +0800

    feat: 初始化 ai-review-book 项目

---
+++ src/test/java/com/damai/aireviewbook/AiReviewBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damai.aireviewbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiReviewBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
